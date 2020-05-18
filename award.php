f5bad621c861c43d5503a1f73ddf84cfc541832f
mackliu <macklun@gmail.com>
2020-05-18 14:55:15 +0800

    完成對獎功能

--- award.php
+++ award.php
@@ -12,31 +12,49 @@
 
 <?php
 
-$award_type=[
-    1=>["特別獎",1],
-    2=>["特獎",2],
-    3=>["頭獎",3],
-    4=>["二獎",3],
-    5=>["三獎",3],
-    6=>["四獎",3],
-    7=>["五獎",3],
-    8=>["六獎",3],
-    9=>["增開六獎",4],
-];
+if(empty($_GET)){
+    echo "請選擇要對獎的項目<a href='invoice.php'>各期獎號</a>";
+    exit();
+}
 
-echo "獎別:".$award_type[$_GET['aw']][0]."<br>";
+$award_type=[
+    1=>["特別獎",1,8],
+    2=>["特獎",2,8],
+    3=>["頭獎",3,8],
+    4=>["二獎",3,7],
+    5=>["三獎",3,6],
+    6=>["四獎",3,5],
+    7=>["五獎",3,4],
+    8=>["六獎",3,3],
+    9=>["增開六獎",4,3],
+];
+$aw=$_GET['aw'];
+echo "獎別:".$award_type[$aw][0]."<br>";
 echo "年份:".$_GET['year']."<br>";
 echo "期別:".$_GET['period']."<br>";
 
 $award_nums=nums("award_number",[
     "year"=>$_GET['year'],
     "period"=>$_GET['period'],
-    "type"=>$award_type[$_GET['aw']][1]
+    "type"=>$award_type[$aw][1]
 ]);
 
 echo "獎號數量:".$award_nums;
+$award_numbers=all("award_number",[
+    "year"=>$_GET['year'],
+    "period"=>$_GET['period'],
+    "type"=>$award_type[$aw][1]
+]) ;
 
-if($award_nums>1){
+echo "<h4>對獎獎號</h4>";
+$t_num=[];
+foreach($award_numbers as $num){
+    echo $num['number']."<br>";
+    $t_num[]=$num['number'];
+}
+
+
+/* if($award_nums>1){
     $award_numbers=all("award_number",[
         "year"=>$_GET['year'],
         "period"=>$_GET['period'],
@@ -52,15 +70,41 @@ if($award_nums>1){
 
 echo "<pre>";
 print_r($award_numbers);
-echo "</pre>";
+echo "</pre>"; */
+echo "<h4>該期發票號碼</h4>";
 
 $invoices=all("invoice",[
     "year"=>$_GET['year'],
     "period"=>$_GET['period'],]);
+
+foreach ($invoices as $ins) {
+
+    foreach($t_num as $tn){
+
+        $len=$award_type[$aw][2];
+
+        
+        $start=8-$len;
+        
+        //針對增開六獎號特別處理
+        if($aw!=9){
+            $target_num=mb_substr($tn,$start,$len);
+        }else{
+            $target_num=$tn;
+        }
+
+        if(mb_substr($ins['number'],$start,$len) == $target_num ){
+            echo "<span style='color:red;font-size:20px'>".$ins['number']."中獎了</span>";
+            echo "<br>";
+        }
+    }
+    
+}
+
+
 ?>
 
-<pre>
-    <?= print_r($invoices);?>
-</pre>
+
+
 </body>
 </html>
